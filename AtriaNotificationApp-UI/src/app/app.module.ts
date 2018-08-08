210c651c22b9badec1a29962486d7c2eba72442f
yusoofash <41569614+yusoofash@users.noreply.github.com>
2018-08-08 22:17:24 +0530

    navbar and aside added

--- AtriaNotificationApp-UI/src/app/app.module.ts
+++ AtriaNotificationApp-UI/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { CardModule } from 'primeng/card';
 
 import { AppComponent } from './app.component';
 import { ContentPageComponent } from './content-page/content-page.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './content-page/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentPageComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
