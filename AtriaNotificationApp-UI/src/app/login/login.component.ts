77da221774e86628929da5bd1e6d2d1864c83400
vinuthayadavs <42105685+vinuthayadavs@users.noreply.github.com>
2018-10-01 17:57:22 +0530

    login validation added

--- AtriaNotificationApp-UI/src/app/login/login.component.ts
+++ AtriaNotificationApp-UI/src/app/login/login.component.ts
@@ -1,27 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
-
-
-
+import { Login } from '../model/login.model';
+import { Router } from '@angular/router';
+import { loginData } from './mock-login';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-  response = '';
-  userdata = {
-    password: '',
-    username: '',
-  };
+    constructor(private router: Router) { }
+    response = '';
+    userdata: Login = {
+        password: '',
+        username: '',
+    };
 
-  ngOnInit() {
-  }
-  get diagnostic() { return JSON.stringify(this.userdata); }// Test }
+    ngOnInit() {
+    }
+
+    validate() {
+        console.log(123);
+        const islogged = loginData.some(val => {
+            if (val.username === this.userdata.username && val.password === this.userdata.password) {
+                return true;
+            }
+        });
+        if (islogged === true) {
+            this.response = 'succesfully loggedin';
+            this.router.navigate(['home']);
+        } else {
+            this.response = 'incorrect username or password';
+        }
+    }
+    get diagnostic() { return JSON.stringify(this.userdata); }
 }
 
 
---
+++ AtriaNotificationApp-UI/src/app/login/mock-login.ts
@@ -0,0 +1,12 @@
+import { Login } from '../model/login.model';
+
+export const loginData: Login[] = [
+    {
+        username: 'yusoofash@gmail.com',
+        password: 'yusoof'
+    },
+    {
+        username: 'vinutha@gmail.com',
+        password: 'vinutha'
+    }
+];
